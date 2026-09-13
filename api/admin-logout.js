ec07d1cdc3dffe929c81e8ecfe964b58b0c8cb76
julien2003-wa <julienngaha0@gmail.com>
2026-09-13 14:24:31 +0100

    Delete api directory

--- api/admin-logout.js
+++
@@ -1,8 +0,0 @@
-import { destroySession, clearSessionCookie, json, method } from '../lib/server.js';
-
-export default async function handler(req, res) {
-  if (!method(req, res, ['POST'])) return;
-  try { await destroySession(req, 'admin'); } catch (err) { console.error(err); }
-  clearSessionCookie(res, 'jl_admin_session');
-  json(res, 200, { ok: true });
-}
